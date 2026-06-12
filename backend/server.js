28e7b76952ed9fe6bf0cf8f69e2c79ef21fce760
PabbathiKeerthi <pabbathikeerthi156@gmail.com>
2026-06-12 12:22:38 +0530

    portfolio_db has changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
-const path = require("path");
 app.use(express.static(path.join(__dirname, "../public")));
 
 const db = mysql.createConnection({
